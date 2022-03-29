17b729d2d042aaeb3b72ac18b25aafef19e7415d
Skitt2 <provaid@yandex.ru>
2022-03-29 08:11:43 +0300

    Добавьте файлы проекта.

---
+++ DZ12/circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ12
+{
+    static class circle
+    {
+        static public double GetLenght(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public double GetSquare(double r)
+        {
+            return Math.PI * r * r;
+        }
+
+        static public double GetPoint(double r, double x, double y) // Значение 1 точка в круге, значение 0 точка вне круга
+        {
+            double c = Math.Sqrt(x * x + y * y);
+
+            if (c>r)
+                return 0;
+            return 1;
+        }
+    }
+}
